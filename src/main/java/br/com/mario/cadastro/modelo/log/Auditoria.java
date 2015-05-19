292d504e13148cee4bd98fa43eef3d81b0efd310
mariocajuru <mariocajuru@yahoo.com.br>
2015-05-19 17:09:03 -0300

    add hibernate envers, add auditoria

---
+++ src/main/java/br/com/mario/cadastro/modelo/log/Auditoria.java
@@ -0,0 +1,130 @@
+package br.com.mario.cadastro.modelo.log;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+import org.hibernate.envers.DefaultRevisionEntity;
+import org.hibernate.envers.RevisionEntity;
+@Entity
+@RevisionEntity(Gravar.class)
+@Table(name="AUDITORIA", schema="log", catalog="cadastro")
+public class Auditoria extends DefaultRevisionEntity implements java.io.Serializable{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -7197655600139767254L;
+
+	/**
+	 * 
+	 */
+	
+	private String login;
+	
+	private String nome;
+	/*@Column(length=900)
+	private String nivelAcesso;*/
+	/*private String cpf;*/
+	private String ip_maquina;
+	/*private String pagina;*/
+	private String nome_maquina;
+	
+	private Integer ip_usuario;
+	
+	
+	
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getIp_maquina() {
+		return ip_maquina;
+	}
+	public void setIp_maquina(String ip_maquina) {
+		this.ip_maquina = ip_maquina;
+	}
+		
+	
+
+	
+	public String getLogin() {
+		return login;
+	}
+	public void setLogin(String login) {
+		this.login = login;
+	}
+	public Integer getIp_usuario() {
+		return ip_usuario;
+	}
+	public void setIp_usuario(Integer ip_usuario) {
+		this.ip_usuario = ip_usuario;
+	}
+	public String getNome_maquina() {
+		return nome_maquina;
+	}
+	public void setNome_maquina(String nome_maquina) {
+		this.nome_maquina = nome_maquina;
+	}
+	
+	public static long getSerialversionuid() {
+		return serialVersionUID;
+	}
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = super.hashCode();
+		result = prime * result
+				+ ((ip_usuario == null) ? 0 : ip_usuario.hashCode());
+		result = prime * result
+				+ ((ip_maquina == null) ? 0 : ip_maquina.hashCode());
+		result = prime * result + ((login == null) ? 0 : login.hashCode());
+		result = prime * result + ((nome == null) ? 0 : nome.hashCode());
+		result = prime * result
+				+ ((nome_maquina == null) ? 0 : nome_maquina.hashCode());
+		return result;
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (!super.equals(obj))
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Auditoria other = (Auditoria) obj;
+		if (ip_usuario == null) {
+			if (other.ip_usuario != null)
+				return false;
+		} else if (!ip_usuario.equals(other.ip_usuario))
+			return false;
+		if (ip_maquina == null) {
+			if (other.ip_maquina != null)
+				return false;
+		} else if (!ip_maquina.equals(other.ip_maquina))
+			return false;
+		if (login == null) {
+			if (other.login != null)
+				return false;
+		} else if (!login.equals(other.login))
+			return false;
+		if (nome == null) {
+			if (other.nome != null)
+				return false;
+		} else if (!nome.equals(other.nome))
+			return false;
+		if (nome_maquina == null) {
+			if (other.nome_maquina != null)
+				return false;
+		} else if (!nome_maquina.equals(other.nome_maquina))
+			return false;
+		return true;
+	}
+	@Override
+	public String toString() {
+		return "Auditoria [getId()=" + getId() + "]";
+	}
+	
+}
---
+++ src/main/java/br/com/mario/cadastro/modelo/log/Gravar.java
@@ -0,0 +1,93 @@
+package br.com.mario.cadastro.modelo.log;
+
+import java.net.InetAddress;
+import java.net.NetworkInterface;
+import java.net.SocketException;
+import java.util.Enumeration;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import org.hibernate.envers.RevisionListener;
+
+import br.com.mario.cadastro.util.ContextoBean;
+import br.com.mario.cadastro.util.ContextoUtil;
+
+
+public class Gravar implements RevisionListener {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -8644920676174993870L;
+	@Getter @Setter private ContextoBean genericBean=ContextoUtil.getContextoBean();
+
+	@Override
+	public void newRevision(Object arg0) {
+		Auditoria auditoria=(Auditoria) arg0;
+		
+		//auditoria.setPagina(this.genericBean.getPaginaAtual());
+
+		//Usuario pessoa= new UsuarioRN().carregar(this.genericBean.getUsuarioLogado().getFunId());
+
+		auditoria.setNome(this.genericBean.getUsuarioLogado().getPessoa().getPesNome());
+		auditoria.setLogin(this.genericBean.getUsuarioLogado().getUseLogin());
+		//auditoria.setNivelAcesso(this.genericBean.getUsuarioLogado().getPermissao().toString());
+		auditoria.setIp_usuario(this.genericBean.getUsuarioLogado().getPesId());
+		
+		 @SuppressWarnings("rawtypes")
+		Enumeration ee = null;
+		try {
+			ee = NetworkInterface.getNetworkInterfaces();
+		} catch (SocketException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		} 
+		boolean achou=false;
+	      while (ee.hasMoreElements()) { 
+	         NetworkInterface i = (NetworkInterface) ee.nextElement(); 
+	         @SuppressWarnings("rawtypes")
+			Enumeration ds = i.getInetAddresses(); 
+	         while (ds.hasMoreElements()) { 
+	            InetAddress myself = (InetAddress) ds.nextElement();
+	            if (!myself.isLoopbackAddress() && myself.isSiteLocalAddress()) {
+	            	auditoria.setIp_maquina(myself.getHostAddress());
+	            	auditoria.setNome_maquina(myself.getHostName());
+	            	achou=true;
+	            	//http://mariojp.wordpress.com/2012/03/11/obtendo-o-endereco-ip-real-da-maquina-na-rede/
+	           // System.out.println("HostName: " + myself.getHostName() + " IP: " + myself.getHostAddress()); 
+	            }
+	            if(achou)break;
+	         } 
+	         if(achou)break;
+	      }
+		
+		
+		/*String ip=new String();
+		String nome_maquina=new String();
+		try {  
+			java.net.InetAddress i = java.net.InetAddress.getLocalHost();  
+			ip = i.getHostAddress();
+			nome_maquina=i.getHostName();
+		}  
+		catch(Exception e){
+			e.printStackTrace();
+			System.out.println("ERRO na auditoria : "+ e.getMessage());
+		}  
+		auditoria.setIp_maquina(ip);
+		auditoria.setNome_maquina(nome_maquina);*/
+	}
+
+	public static long getSerialversionuid() {
+		return serialVersionUID;
+	}
+
+	@Override
+	public String toString() {
+		return "Gravar [getPaginaAtual()=" + this.genericBean.getPaginaAtual() + ", getClass()="
+				+ getClass() + ", hashCode()=" + hashCode() + ", toString()="
+				+ this.genericBean.toString() + "]";
+	}
+	
+
+}
