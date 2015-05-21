2e1dd5c2fc26888aac67e5bdc24cd0cb10f2187e
mariocajuru <mariocajuru@yahoo.com.br>
2015-05-21 16:05:18 -0300

    add itens no grafico na pagina principal

--- src/main/webapp/resources/js/plugins/morris/morris-data.js
+++ src/main/webapp/resources/js/plugins/morris/morris-data.js
@@ -15,63 +15,18 @@ $(function() {
 			// Area Chart
 		    Morris.Area({
 		        element: 'morris-area-chart',
-		        data: [{
-		            period: '2010 Q1',
-		            iphone: 2666,
-		            ipad: null,
-		            itouch: 2647
-		        }, {
-		            period: '2010 Q2',
-		            iphone: 2778,
-		            ipad: 2294,
-		            itouch: 2441
-		        }, {
-		            period: '2010 Q3',
-		            iphone: 4912,
-		            ipad: 1969,
-		            itouch: 2501
-		        }, {
-		            period: '2010 Q4',
-		            iphone: 3767,
-		            ipad: 3597,
-		            itouch: 5689
-		        }, {
-		            period: '2011 Q1',
-		            iphone: 6810,
-		            ipad: 1914,
-		            itouch: 2293
-		        }, {
-		            period: '2011 Q2',
-		            iphone: 5670,
-		            ipad: 4293,
-		            itouch: 1881
-		        }, {
-		            period: '2011 Q3',
-		            iphone: 4820,
-		            ipad: 3795,
-		            itouch: 1588
-		        }, {
-		            period: '2011 Q4',
-		            iphone: 15073,
-		            ipad: 5967,
-		            itouch: 5175
-		        }, {
-		            period: '2012 Q1',
-		            iphone: 10687,
-		            ipad: 4460,
-		            itouch: 2028
-		        }, {
-		            period: '2012 Q2',
-		            iphone: 8432,
-		            ipad: 5713,
-		            itouch: 1791
-		        }],
-		        xkey: 'period',
-		        ykeys: ['iphone', 'ipad', 'itouch'],
-		        labels: ['iPhone', 'iPad', 'iPod Touch'],
-		        pointSize: 2,
-		        hideHover: 'auto',
-		        resize: true
+		               data: [
+		                      { y: '2009', a: 75,  b: 65 },
+		                      { y: '2010', a: 50,  b: 40 },
+		                      { y: '2011', a: 75,  b: 65 },
+		                      { y: '2012', a: 100, b: 90 },  
+		                      { y: '2013', a: 100, b: 90 },
+		                      { y: '2014', a: 75,  b: 65 },
+		                      { y: '2015', a: 50,  b: 40 },
+		                    ],
+		                    xkey: 'y',
+		                    ykeys: ['a', 'b'],
+		                    labels: ['Clientes Pessoa Jurídica', 'Clientes Pessoa Física']
 		    });
 
 
@@ -79,13 +34,13 @@ $(function() {
 			    Morris.Donut({
 			        element: 'morris-donut-chart',
 			        data: [{
-			            label: "Download Sales",
-			            value: 12
+			            label: "Clientes Pessoa Jurídica",
+			            value: 120
 			        }, {
-			            label: "In-Store Sales",
-			            value: 30
+			            label: "Clientes Pessoa Física",
+			            value: 300
 			        }, {
-			            label: "Mail-Order Sales",
+			            label: "Vendedores",
 			            value: 20
 			        }],
 			        resize: true
